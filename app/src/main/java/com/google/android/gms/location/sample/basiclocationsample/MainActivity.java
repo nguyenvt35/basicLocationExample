394c4f61012906d0fd6224b464e32049e13d4793
nguyenvt35 <69822014+nguyenvt35@users.noreply.github.com>
2021-12-17 13:56:36 -0500

    Allowed Permissions to be Requested

--- app/src/main/java/com/google/android/gms/location/sample/basiclocationsample/MainActivity.java
+++ app/src/main/java/com/google/android/gms/location/sample/basiclocationsample/MainActivity.java
@@ -83,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
 
         if (!checkPermissions()) {
-            requestPermissions();
+            requestPermissions(); //uncommented
         } else {
             getLastLocation();
         }
